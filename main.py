29f3330c53532d699010b48241345583dbf7a43b
Maxim6321ua <riznikmaksim60@gmail.com>
2025-12-15 11:42:15 +0200

    додав команду gift яка може рандомно додати кількість монет від 100 до 1000  а ще додані повідомлення якщо в вас менше випало ніж 500 то тоді таке повідомлення а якщо більше то тоді інше

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
+import random
+
 server_economy = {
     'Admin': 999999,
     'Steve': 0,
     'Alex': 150
+}
+gift_text = {
+    "money_loser": "Чомусь ти мало отримав монет, напевно твій дід мороз купив оперативну пам'ять для свого ПК",
+    "money_winner": "Вау! Тобі дуже пощастило, ти отримав багато монет, напевно твій дід мороз багато заробляє"
 }
 
 print("---  MineBank Console v1.0  ---")
@@ -11,6 +17,7 @@ print("  !balance [нік]          - Перевірити баланс грав
 print("  !give [нік] [сума]      - (Admin) Видати гроші гравцю")
 print("  !pay [хто] [кому] [сума]- Переказ грошей між гравцями")
 print("  !top                    - Показати всіх гравців")
+print("  !gift [нік]             - Отримати випадковий подарунок (100-1000 монет)")
 print("  !exit                   - Вийти з консолі")
 print("--------------------------------------")
 
@@ -97,6 +104,21 @@ while True:
         for name, money in server_economy.items():
             print(f"- {name}: {money} $")
         print("=========================")
+    elif command == '!gift':
+        if len(parts) > 1:
+            nickname = parts[1]
+            if nickname in server_economy:
+                gift_amount = random.randint(100, 1000)
+                server_economy[nickname] += gift_amount
+                if gift_amount < 500:
+                    print(gift_text["money_loser"])
+                else:
+                    print(gift_text["money_winner"])
+                print(f"Гравець {nickname} отримав подарунок: {gift_amount} монет! Новий баланс: {server_economy[nickname]}")
+            else:
+                print(f"Помилка: Гравця {nickname} не знайдено.")
+        else:
+            print("Використання: !gift [нікнейм]")
 
     elif command == '!exit':
         print("Роботу завершено. До побачення!")
